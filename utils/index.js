f69934b3d54a93396a8ef047cd6089dfac83cc16
juggernaut03 <gauravpawar904@gmail.com>
2025-01-30 07:42:16 +0530

    delete

--- utils/index.js
+++
@@ -1,2 +0,0 @@
-// src/config/index.js
-const apiKey = process.env.ANTHROPIC_API_KEY;
--- utils/networkUtils.js
+++
@@ -1,7 +0,0 @@
-// src/utils/networkUtils.js
-import NetInfo from '@react-native-community/netinfo';
-
-export const checkNetworkConnection = async () => {
-  const networkState = await NetInfo.fetch();
-  return networkState.isConnected && networkState.isInternetReachable;
-};
